700e259a9f550b39cf698de97582fca93728ba72
David Dudas <david.dudas@intuitial.com>
2025-05-27 16:20:12 -0700

    minor func change plus changeset file

--- packages-python/common_grants_sdk/setup.py
+++ packages-python/common_grants_sdk/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="common_grants_sdk",
-    version="0.2.0",
+    version="0.3.0",
     description="Python implementation of CommonGrants Protocol",
     author="HHS",
     packages=find_packages(),
